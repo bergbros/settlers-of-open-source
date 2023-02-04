60c166127e8d1499c46dbadfae281acc2437cc14
berg44 <dvdbrgsn@gmail.com>
2023-02-04 15:12:59 -0700

    Added "tiles" for hexes and numbers
    Added additional resource names
    Changed "w" water to "/"; "w" is now wood;
    Added a numberPile and tilePile to represent the relative frequencies of various tiles, and a '?' to the map to represent a randomizer for the location of land tiles.
    Resource hexes now have numbers on them (not desert or water)

--- soos-client/src/App.tsx
+++ soos-client/src/App.tsx
@@ -2,21 +2,21 @@ import { ReactNode, useState } from 'react';
 import './App.scss';
 
 const HexWidth = 100, HexHeight = 120;
-const BoardWidth = 8, BoardHeight = 7;
-const Terrain = [
-  [ 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w' ],
-  [ 'w', 'w', 'd', 'd', 'd', 'd', 'w', 'w' ],
-  [ 'w', 'w', 'd', 'g', 'g', 'g', 'd', 'w' ],
-  [ 'w', 'd', 'g', 'g', 'g', 'g', 'd', 'w' ],
-  [ 'w', 'w', 'd', 'g', 'g', 'g', 'd', 'w' ],
-  [ 'w', 'w', 'd', 'd', 'd', 'd', 'w', 'w' ],
-  [ 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w' ],
-];
+const BoardWidth = 7, BoardHeight = 7;
 
 export default function App() {
   const board = [];
   const [clickStatement, setCS] = useState('no clicks');
-
+  let Terrain = [
+    [ '/', '/', '/', '/', '/', '/', '/' ],
+    [ '/', '/', '?', '?', '?', '/', '/' ],
+    [ '/', '/', '?', '?', '?', '?', '/' ],
+    [ '/', '?', '?', '?', '?', '?', '/' ],
+    [ '/', '/', '?', '?', '?', '?', '/' ],
+    [ '/', '/', '?', '?', '?', '/', '/' ],
+    [ '/', '/', '/', '/', '/', '/', '/' ],
+  ];
+  
   function addSettleSpot(ssXCoord:number, ssYCoord:number,ssKey:string){
     return (
           <div className={`SettleSpot`} style={{
@@ -45,12 +45,40 @@ export default function App() {
       </div>
     );
   }
+  //three brick/ore, four wood/grain/sheep, one desert; 19 tiles
+  let tilePile = ['b','b','b','o','o','o','w','w','w','w','g','g','g','g','s','s','s','s','d'];
+  let numberPile = [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12];
 
   for (let y = 0; y < BoardHeight; y++) {
     const isOffset: boolean = y % 2 === 1;
-
     const row = [];
     for (let x = 0; x < BoardWidth; x++) {
+      
+      let terrainClass = '';
+      switch (Terrain[y][x]) {
+        case '/': terrainClass = 'water'; break;
+        case '?':
+          let tileIndex = 0;
+          let newTile ='d'
+          if (tilePile.length>1){
+            tileIndex = Math.floor(Math.random() * tilePile.length);
+            newTile = tilePile[tileIndex];
+          }
+          if (tilePile.length===1) newTile = tilePile[0];
+          tilePile.splice(tileIndex,1)
+          //console.log(`grabbed ${tileIndex+1} of ${tilePile.length}, tile remaining:${tilePile}`);
+          switch(newTile){
+            case 'b': terrainClass = 'brick'; break;
+            case 'd': terrainClass = 'desert';break;
+            case 'g': terrainClass = 'grain'; break;
+            case 'o': terrainClass = 'ore';   break;
+            case 's': terrainClass = 'sheep'; break;
+            case 'w': terrainClass = 'wood';  break;    
+          }
+          Terrain[y][x] = newTile;
+          break;
+      }
+
       let xCoord = x * HexWidth;
       if (isOffset) {
         xCoord += HexWidth * .5;
@@ -58,14 +86,16 @@ export default function App() {
       xCoord+=55;
       const yCoord = y * HexHeight * .75+55;
 
-      let terrainClass = '';
-      switch (Terrain[y][x]) {
-        case 'w': terrainClass = 'water'; break;
-        case 'd': terrainClass = 'desert'; break;
-        case 'g': terrainClass = 'grass'; break;
+      let numberIndex = null;
+      let tileNumber = null;
+      if (Terrain[y][x]==='/' ||Terrain[y][x]==='d') {
+      } else {
+        numberIndex = numberPile.length>1 ? Math.floor(Math.random() * numberPile.length) : 0;
+        tileNumber = numberPile.length>0 ? numberPile[numberIndex] : 0;
+        if (numberPile.length>0) numberPile.splice(numberIndex,1);
+        console.log(`grabbed ${numberIndex+1} of ${numberPile.length}, tile remaining:${numberPile}`);
       }
-
-      row.push(
+      row.push( 
         <div 
           className={`Hex ${terrainClass}`} style={{
             left: xCoord + 'px',
@@ -73,64 +103,51 @@ export default function App() {
           }}
           key = {`h:${x},${y}`}
           >
+            <div className="tileNumber">{tileNumber}</div>
         </div>
       );
       
       const ssRadius = 20/2;
+      let ssXCoord =  0;
+      let ssYCoord = 0;
       //add top left settlement spot on each hex
-      let ssXCoord = xCoord - ssRadius ;
-      let ssYCoord = yCoord - ssRadius+ HexHeight*.25;
-      row.push(addSettleSpot(ssXCoord,ssYCoord,`s:${x},${y},6`));
-
+      row.push(addSettleSpot(xCoord - ssRadius,yCoord - ssRadius+ HexHeight*.25,`s:${x},${y},6`));
       //add top center settlement spot on each hex
-      ssXCoord = xCoord - ssRadius + HexWidth*0.5 ;
-      ssYCoord = yCoord - ssRadius;
-      row.push(addSettleSpot(ssXCoord,ssYCoord,`s:${x},${y},1`));
-
+      row.push(addSettleSpot(xCoord - ssRadius + HexWidth*0.5,yCoord - ssRadius,`s:${x},${y},1`));
       //add right corners for end of row hexes
       if(x === BoardWidth-1){
-        ssXCoord = xCoord - ssRadius + HexWidth;
-        ssYCoord = yCoord - ssRadius + HexHeight*0.25;
-        row.push(addSettleSpot(ssXCoord,ssYCoord,`s:${x},${y},2`));
-        ssXCoord = xCoord - ssRadius + HexWidth;
-        ssYCoord = yCoord - ssRadius+HexHeight*0.75;
-        row.push(addSettleSpot(ssXCoord,ssYCoord,`s:${x},${y},3`));
-        
+        row.push(addSettleSpot(xCoord - ssRadius + HexWidth,yCoord - ssRadius + HexHeight*0.25,`s:${x},${y},2`));
+        row.push(addSettleSpot(xCoord - ssRadius + HexWidth,yCoord - ssRadius+HexHeight*0.75,`s:${x},${y},3`));
       }
 
       //add bottom left for the first hexes on each row and the entire last row
-      if(y===BoardHeight-1||x===0){
-        ssXCoord = xCoord - ssRadius;
-        ssYCoord = yCoord - ssRadius + HexHeight*0.75;
-        row.push(addSettleSpot(ssXCoord,ssYCoord,`s:${x},${y},4`));
-      }
-
+      if(y===BoardHeight-1||x===0){ row.push(addSettleSpot(xCoord - ssRadius,yCoord - ssRadius + HexHeight*0.75,`s:${x},${y},4`)); }
       //add bottom centers for the last row of hexes
-      if(y === BoardHeight-1){
-        ssXCoord = xCoord - ssRadius + HexWidth*0.5;
-        ssYCoord = yCoord - ssRadius+HexHeight;
-        row.push(addSettleSpot(ssXCoord,ssYCoord,`s:${x},${y},5`));
-      }
+      if(y === BoardHeight-1){row.push(addSettleSpot(xCoord - ssRadius + HexWidth*0.5,yCoord - ssRadius+HexHeight,`s:${x},${y},5`));}
 
       //Add the Roads!
       const RoadWidth = 12/2
-      row.push(addRoadSpot(xCoord,yCoord,"neSlant",`r:${x},${y},6`));
       row.push(addRoadSpot(xCoord+0.5*HexWidth,yCoord,"nwSlant",`r:${x},${y},1`));
       row.push(addRoadSpot(xCoord-RoadWidth,yCoord+0.25*HexHeight+ssRadius+5,"vertical",`r:${x},${y},5`));
-      if(x === 0){
-        row.push(addRoadSpot(xCoord,yCoord+HexHeight*0.75,"nwSlant",`r:${x},${y},4`));
-      }
+      row.push(addRoadSpot(xCoord,yCoord,"neSlant",`r:${x},${y},6`));
+      
       if(x === BoardWidth-1){
         row.push(addRoadSpot(xCoord-RoadWidth+HexWidth,yCoord+0.25*HexHeight+ssRadius+5,"vertical",`r:${x},${y},2`));
+      }
+      if(y === BoardHeight - 1 || x === BoardWidth-1){
         row.push(addRoadSpot(xCoord+0.5*HexWidth,yCoord+HexHeight*0.75,"neSlant",`r:${x},${y},3`));
       }
-      if(y === BoardHeight - 1){
-        row.push(addRoadSpot(xCoord+0.5*HexWidth,yCoord+HexHeight*0.75,"neSlant",`r:${x},${y},3`));
+      if(x === 0 || y === BoardHeight - 1 ) { 
         row.push(addRoadSpot(xCoord,yCoord+HexHeight*0.75,"nwSlant",`r:${x},${y},4`));
       }
     }
     board.push(row);
   }
+
+  console.log(`Terrain: ${Terrain}`);
+          
+
+
 
   return (
     <div className="App">
@@ -138,8 +155,4 @@ export default function App() {
       {clickStatement}
     </div>
   );
-
-  
-  
-
 }

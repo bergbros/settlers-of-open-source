c03d4a0f4336eaf819ad0e55cd6596c51796f262
berg44 <dvdbrgsn@gmail.com>
2023-02-04 16:05:43 -0700

    Added Frequencies, made settlement & road placement generic
    Settlements and roads should be able to accommodate any map size or arrangement now!

--- soos-client/src/App.tsx
+++ soos-client/src/App.tsx
@@ -7,17 +7,20 @@ const BoardWidth = 7, BoardHeight = 7;
 export default function App() {
   const board = [];
   const [clickStatement, setCS] = useState('no clicks');
+  let settleSpotCount = 0; let roadSpotCount=0;
   let Terrain = [
-    [ '/', '/', '/', '/', '/', '/', '/' ],
-    [ '/', '/', '?', '?', '?', '/', '/' ],
-    [ '/', '/', '?', '?', '?', '?', '/' ],
+    [ 'e', 'e', '/', '/', '/', '/', 'e' ],
+    [ 'e', '/', '?', '?', '?', '/', 'e' ],
+    [ 'e', '/', '?', '?', '?', '?', '/' ],
     [ '/', '?', '?', '?', '?', '?', '/' ],
-    [ '/', '/', '?', '?', '?', '?', '/' ],
-    [ '/', '/', '?', '?', '?', '/', '/' ],
-    [ '/', '/', '/', '/', '/', '/', '/' ],
+    [ 'e', '/', '?', '/', '/', '?', '/' ],
+    [ 'e', '/', '?', '?', '?', '/', 'e' ],
+    [ 'e', 'e', '/', '/', '/', '/', 'e' ],
   ];
-  
+  let terrFrequency = [];
+
   function addSettleSpot(ssXCoord:number, ssYCoord:number,ssKey:string){
+    settleSpotCount++;
     return (
           <div className={`SettleSpot`} style={{
             left: ssXCoord + 'px',
@@ -31,6 +34,7 @@ export default function App() {
   }
 
   function addRoadSpot(rsXCoord:number, rsYCoord:number, rDirection:string, rsKey:string){
+    roadSpotCount++;
     return (
       <div 
       className={`RoadSpot ${rDirection}`} 
@@ -52,10 +56,11 @@ export default function App() {
   for (let y = 0; y < BoardHeight; y++) {
     const isOffset: boolean = y % 2 === 1;
     const row = [];
+    const freqRow = [];
     for (let x = 0; x < BoardWidth; x++) {
-      
       let terrainClass = '';
       switch (Terrain[y][x]) {
+        case 'e': terrainClass = 'empty'; break;
         case '/': terrainClass = 'water'; break;
         case '?':
           let tileIndex = 0;
@@ -66,7 +71,6 @@ export default function App() {
           }
           if (tilePile.length===1) newTile = tilePile[0];
           tilePile.splice(tileIndex,1)
-          //console.log(`grabbed ${tileIndex+1} of ${tilePile.length}, tile remaining:${tilePile}`);
           switch(newTile){
             case 'b': terrainClass = 'brick'; break;
             case 'd': terrainClass = 'desert';break;
@@ -88,13 +92,14 @@ export default function App() {
 
       let numberIndex = null;
       let tileNumber = null;
-      if (Terrain[y][x]==='/' ||Terrain[y][x]==='d') {
+      if (Terrain[y][x]==='/' ||Terrain[y][x]==='d'||Terrain[y][x]==='e') {
       } else {
         numberIndex = numberPile.length>1 ? Math.floor(Math.random() * numberPile.length) : 0;
         tileNumber = numberPile.length>0 ? numberPile[numberIndex] : 0;
         if (numberPile.length>0) numberPile.splice(numberIndex,1);
-        console.log(`grabbed ${numberIndex+1} of ${numberPile.length}, tile remaining:${numberPile}`);
       }
+
+      freqRow.push(tileNumber);
       row.push( 
         <div 
           className={`Hex ${terrainClass}`} style={{
@@ -106,44 +111,43 @@ export default function App() {
             <div className="tileNumber">{tileNumber}</div>
         </div>
       );
-      
-      const ssRadius = 20/2;
-      let ssXCoord =  0;
-      let ssYCoord = 0;
-      //add top left settlement spot on each hex
-      row.push(addSettleSpot(xCoord - ssRadius,yCoord - ssRadius+ HexHeight*.25,`s:${x},${y},6`));
-      //add top center settlement spot on each hex
-      row.push(addSettleSpot(xCoord - ssRadius + HexWidth*0.5,yCoord - ssRadius,`s:${x},${y},1`));
-      //add right corners for end of row hexes
-      if(x === BoardWidth-1){
-        row.push(addSettleSpot(xCoord - ssRadius + HexWidth,yCoord - ssRadius + HexHeight*0.25,`s:${x},${y},2`));
-        row.push(addSettleSpot(xCoord - ssRadius + HexWidth,yCoord - ssRadius+HexHeight*0.75,`s:${x},${y},3`));
-      }
 
-      //add bottom left for the first hexes on each row and the entire last row
-      if(y===BoardHeight-1||x===0){ row.push(addSettleSpot(xCoord - ssRadius,yCoord - ssRadius + HexHeight*0.75,`s:${x},${y},4`)); }
-      //add bottom centers for the last row of hexes
-      if(y === BoardHeight-1){row.push(addSettleSpot(xCoord - ssRadius + HexWidth*0.5,yCoord - ssRadius+HexHeight,`s:${x},${y},5`));}
-
-      //Add the Roads!
-      const RoadWidth = 12/2
-      row.push(addRoadSpot(xCoord+0.5*HexWidth,yCoord,"nwSlant",`r:${x},${y},1`));
-      row.push(addRoadSpot(xCoord-RoadWidth,yCoord+0.25*HexHeight+ssRadius+5,"vertical",`r:${x},${y},5`));
-      row.push(addRoadSpot(xCoord,yCoord,"neSlant",`r:${x},${y},6`));
+      //add settlement spots and roads if necessary!
+      if(Terrain[y][x]!=='e' && Terrain[y][x]!=='/'){
+        const ssRadius = 20/2;
+        let ssXCoord =  0;
+        let ssYCoord = 0;
+        //add top left settlement spot on each hex
+        row.push(addSettleSpot(xCoord - ssRadius,yCoord - ssRadius+ HexHeight*.25,`s:${x},${y},6`));
+        //add top center settlement spot on each hex
+        row.push(addSettleSpot(xCoord - ssRadius + HexWidth*0.5,yCoord - ssRadius,`s:${x},${y},1`));
+        //add right corners for end of row hexes
+        if(Terrain[y][x+1] === '/' && y<BoardHeight/2) row.push(addSettleSpot(xCoord - ssRadius + HexWidth,yCoord - ssRadius + HexHeight*0.25,`s:${x},${y},2`));
+        if(Terrain[y][x+1] === '/' && Terrain[y+1][x+(y%2)] === '/') row.push(addSettleSpot(xCoord - ssRadius + HexWidth,yCoord - ssRadius+HexHeight*0.75,`s:${x},${y},3`));  
+        //add bottom left for the first hexes on each row and the last row of hexes
+        if(Terrain[y+1][x-((y+1)%2)]==='/'){ row.push(addSettleSpot(xCoord - ssRadius,yCoord - ssRadius + HexHeight*0.75,`s:${x},${y},4`));}
+        //add bottom center for the entire last row
+        if(Terrain[y+1][x+(y%2)]==='/'){row.push(addSettleSpot(xCoord - ssRadius + HexWidth*0.5,yCoord - ssRadius+HexHeight,`s:${x},${y},5`));}
+  
+        //Add the Roads!
+        const RoadWidth = 12/2
+        row.push(addRoadSpot(xCoord+0.5*HexWidth,yCoord,"nwSlant",`r:${x},${y},1`));
+        row.push(addRoadSpot(xCoord-RoadWidth,yCoord+0.25*HexHeight+ssRadius+5,"vertical",`r:${x},${y},5`));
+        row.push(addRoadSpot(xCoord,yCoord,"neSlant",`r:${x},${y},6`));
+        
+        if(Terrain[y][x+1] === '/') 
+          row.push(addRoadSpot(xCoord-RoadWidth+HexWidth,yCoord+0.25*HexHeight+ssRadius+5,"vertical",`r:${x},${y},2`));
+        if(Terrain[y+1][x+y%2] === '/')
+          row.push(addRoadSpot(xCoord+0.5*HexWidth,yCoord+HexHeight*0.75,"neSlant",`r:${x},${y},3`));
+        if(Terrain[y+1][x+y%2-1] === '/') 
+          row.push(addRoadSpot(xCoord,yCoord+HexHeight*0.75,"nwSlant",`r:${x},${y},4`));
+        
+      }
       
-      if(x === BoardWidth-1){
-        row.push(addRoadSpot(xCoord-RoadWidth+HexWidth,yCoord+0.25*HexHeight+ssRadius+5,"vertical",`r:${x},${y},2`));
-      }
-      if(y === BoardHeight - 1 || x === BoardWidth-1){
-        row.push(addRoadSpot(xCoord+0.5*HexWidth,yCoord+HexHeight*0.75,"neSlant",`r:${x},${y},3`));
-      }
-      if(x === 0 || y === BoardHeight - 1 ) { 
-        row.push(addRoadSpot(xCoord,yCoord+HexHeight*0.75,"nwSlant",`r:${x},${y},4`));
-      }
     }
+    terrFrequency.push(freqRow)
     board.push(row);
   }
-
   console.log(`Terrain: ${Terrain}`);
           
 

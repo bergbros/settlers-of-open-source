69225995218694caa9e77fa757da6ea44584122d
Jan Bergeson <jphbergeson@gmail.com>
2023-06-27 22:29:25 -0600

    Try different robber image

--- soos-client/src/components/robber/Robber.tsx
+++ soos-client/src/components/robber/Robber.tsx
@@ -1,5 +1,5 @@
 import { Game } from 'soos-gamelogic';
-import RobberImg from '/Robber.png';
+import RobberImg from '/cloak-dagger.svg';
 import { hexCoordsToPixels, HexHeight, HexWidth } from '../../utils';
 import './Robber.scss';
 
@@ -10,7 +10,7 @@ export type RobberProps = {
 export const Robber = (props: RobberProps) => {
   const { game } = props;
   let { x, y } = hexCoordsToPixels(game.robberLocation);
-  x += HexWidth * 0.7;
+  x += HexWidth * 0.55;
   y += HexHeight * 0.3;
   return (
     <img

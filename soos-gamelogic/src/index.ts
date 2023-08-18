2e03368146283396ace147be40bfaafd9c349734
talmage89 <talmage.bergeson@gmail.com>
2023-08-17 21:35:44 -0600

    exported buildAction and hydrateBuildAction

--- soos-gamelogic/src/index.ts
+++ soos-gamelogic/src/index.ts
@@ -3,7 +3,7 @@ import GameHex from './game-hex.js';
 import GameTown from './game-town.js';
 import GameRoad from './game-road.js';
 import GamePlayer from './game-player.js';
-import { actionToString, actionCostString, AllBuildActionTypes, BuildActionType } from './build-actions.js';
+import { actionToString, actionCostString, AllBuildActionTypes, BuildActionType, hydrateBuildAction, BuildAction } from './build-actions.js';
 import { AllResourceTypes, ResourceType, TerrainType, resourceToString, resourceToLand, resourceToLetter, resourceToSymbol, isSeaType } from './terrain-type.js';
 import HexCoords, { HexDirection } from './utils/hex-coords.js';
 import VertexCoords, { VertexDirection } from './utils/vertex-coords.js';
@@ -14,6 +14,7 @@ export {
   actionCostString,
   AllBuildActionTypes,
   AllResourceTypes,
+  BuildAction,
   BuildActionType,
   EdgeCoords,
   Game,
@@ -25,6 +26,7 @@ export {
   GameTown,
   HexCoords,
   HexDirection,
+  hydrateBuildAction,
   isSeaType,
   resourceToLand,
   resourceToLetter,

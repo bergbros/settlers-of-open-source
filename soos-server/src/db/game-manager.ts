befbd98a4ae51d121150c6c3aba80ee0aede1551
gbergeson <23108518+gbergeson@users.noreply.github.com>
2023-08-27 15:31:38 +0300

    GameManager refactor to be more feature complete

--- soos-server/src/db/game-manager.ts
+++ soos-server/src/db/game-manager.ts
@@ -1,35 +1,128 @@
 import { Game } from 'soos-gamelogic';
+import ServerAction from '../server-action.js';
+
 import { generateGameCode } from './utils.js';
+import { DataManager } from './data-manager.js';
+import { userManager } from './user-manager.js';
 
-let gameSet: Set<string> = new Set<string>();
+import { RemoteSocket, Socket } from 'socket.io';
 
-type PlayerDataFields = {
+export type PlayerDataFields = {
   userID: string,
   playerIndex: number,
   connected: boolean
 }
 
-type GameStorage = {
+export type GameStorage = {
   gamecode: string,
   game: Game,
-  players: PlayerDataFields[]
+  players: DataManager,
+  launched: boolean,
+  premoveActions: ServerAction[]
 }
 
-let gameTable: GameStorage[] = [];
-// TODO if debug gen testgamecode
+class GameManager {
+  private gameTable: DataManager;
 
-export var gameManager = {
-  games: gameSet,
+  public constructor() {
+    this.gameTable = new DataManager();
+  }
 
-  createGame: () => {
-    let gamecode = generateGameCode();
-    gameSet.add(gamecode);
+  public getGame(gamecode: string) {
+    var game = this.gameTable.getObjectByAttr('gamecode', gamecode) as GameStorage;
+    if (game) {
+      return game;
+    } else {
+      return null;
+    }
+  }
+
+  public gameExists(gamecode: string) {
+    if (this.getGame(gamecode)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public createGame() {
+    var gamecode = generateGameCode();
+    var createdGame: GameStorage = {
+      gamecode: gamecode,
+      game: new Game({ debugAutoPickSettlements: false }),
+      players: new DataManager(),
+      premoveActions: [],
+      launched: false
+    }
+    this.gameTable.addObject(createdGame);
+
     return gamecode;
-  },
-  deleteGame: (gamecode: string) => {
-    gameSet.delete(gamecode);
-  },
-  gameExists: (gamecode: string) => {
-    return (gameSet.has(gamecode));
+  }
+
+  public launchGame(gamecode: string, playerSockets: any[]) {
+    var game = this.getGame(gamecode);
+    if (!game)
+      throw new Error();
+
+    let playerIndex = 0;
+    let players: string[] = [];
+
+    // These might actually be RemoteSockets (hence the any[] above) but it shouldn't matter here.
+    playerSockets.forEach((playerSocket: Socket) => {
+      var player = userManager.getUserBySocketID(playerSocket.id);
+      if (!player)
+        throw new Error(); // unassociated socket
+      // Possibly also check that player.userID == socket.data.userID
+
+      var pdf: PlayerDataFields = {
+        userID: player.userID,
+        playerIndex: playerIndex,
+        connected: true
+      }
+
+      game?.players.addObject(pdf);
+      players.push(player.userID);
+    });
+
+    console.log(`Launching game ${gamecode} with players ${players}`)
+  }
+
+  public updateGame(gamecode: string, game: Game) {
+    console.log('Updating game ' + gamecode);
+    var gameStorageUnit = this.getGame(gamecode);
+    if (!gameStorageUnit)
+      return false;
+
+    gameStorageUnit.game = game;
+    return true;
+  }
+
+  public getPlayerIndexBySocketID(gamecode: string, socketID: string): number {
+    var user = userManager.getUserBySocketID(socketID);
+    if (!user)
+      throw new Error('Socket ' + socketID + ' not associated with any userID');
+
+    var game = this.getGame(gamecode);
+    var player = game?.players.getObjectByAttr('userID', user.userID) as PlayerDataFields;
+
+    if (!player)
+      throw new Error('User ' + user.userID + ' not in game');
+
+    return player.playerIndex;
+  }
+
+  public gameActive(gamecode: string): boolean {
+    var game = this.getGame(gamecode);
+    if (!game)
+      return false;
+    return game.launched;
+  }
+
+  public deleteGame(gamecode: string) {
+    var result = this.gameTable.removeObjectByAttr('gamecode', gamecode);
+    if (result)
+      return true;
   }
 }
+
+export let gameManager = new GameManager();

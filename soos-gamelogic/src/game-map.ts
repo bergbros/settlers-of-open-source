3f5a3a9110044ccbd3259f7a9dc0a007f6c1e158
berg44 <dvdbrgsn@gmail.com>
2023-08-31 22:51:57 -0600

    fixed buildable road location function

--- soos-gamelogic/src/game-map.ts
+++ soos-gamelogic/src/game-map.ts
@@ -288,13 +288,22 @@ export default class GameMap {
   buildableRoadLocations(playerIdx: number): EdgeCoords[] {
     const roadLocations: { [edgeCoordsStr: string]: EdgeCoords } = {};
     const visitedVertices: { [vertexCoordsStr: string]: true } = {};
-    for (const town of this.towns) {
-      if (town.coords)
-        this.buildableRoadLocationsRecursive(playerIdx, town.coords, roadLocations, visitedVertices);
+    for (const road of this.roads) {
+      const roadCoordsStr = road.coords.toString();
+      if (road.coords && road.playerIdx === playerIdx && !roadLocations[roadCoordsStr]) {
+        roadLocations[roadCoordsStr] = road.coords;
+        const bothTowns = this.getTowns(road);
+        for (const town of bothTowns) {
+          if (town && town.coords) {
+            this.buildableRoadLocationsRecursive(playerIdx, town.coords, roadLocations, visitedVertices);
+          }
+        }
+      }
     }
 
     return Object.values(roadLocations);
   }
+
 
   private buildableRoadLocationsRecursive(
     playerIdx: number,
@@ -307,8 +316,6 @@ export default class GameMap {
     visitedVertices[vertexCoords.toString()] = true;
 
     const town = this.townAt(vertexCoords);
-    if (town?.playerIdx !== playerIdx)
-      return;
 
     const egressRoads = this.getRoads(vertexCoords);
     for (const road of egressRoads) {
@@ -323,7 +330,7 @@ export default class GameMap {
           // continue on the other side
           const bothTowns = this.getTowns(road);
           const otherTown = bothTowns.find(t => !t.coords?.equals(vertexCoords));
-          if (otherTown?.coords)
+          if (otherTown?.coords && otherTown?.playerIdx === playerIdx)
             this.buildableRoadLocationsRecursive(playerIdx, otherTown.coords, roadLocations, visitedVertices);
         }
       }
